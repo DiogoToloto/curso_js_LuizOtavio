a5068c6d9f13cfea6ab98d0d9adfd3af92df52da
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-02-14 05:32:23 -0300

    condiçoes

---
+++ aula10_operadores/index.js
@@ -0,0 +1,14 @@
+/*
+operadores de comparação
+> maior que
+< menor que
+>= maior ou igual que
+<= menor ou igual que
+== igual (verifica só valor) nao utilize ele*
+=== igual estrito (verifica valor e tipo)
+!= diferente (verifica só valor) nao utilize ele*
+!== diferente estrito (verifica valor e tipo)
+*/
+
+const comparação = 2 !== '2';
+console.log(comparação)
---
+++ aula11_operadores_logicos/index.js
@@ -0,0 +1,7 @@
+/*
+operadores logicos
+&& == E / todas as expressoes tem que ser verdadeiras
+|| == ou / uma das duas devem ser verdadeiras
+! == NAO troca o valor EX: true para false
+*/
+console.log()
