0e4f49e2be4b7a9f8c2e22801c04569d3a06f9e8
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-02-28 22:03:26 -0300

    fazendo um timer

--- ex14_timer/assets/js/main.js
+++ ex14_timer/assets/js/main.js
@@ -1,13 +1,48 @@
-function iniciar() {
-    let timer = document.querySelector('.texto')
-    let data = new Date()
-    let tempoLimite = data.getDate()
-    const tempo = setInterval(function() {
-        for (let i = 0; i < tempoLimite;) {
+function relogio(){
 
-        }
-    }, 1000)
-    
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-br', {
+        hour12: false,
+        timeZone: 'UTC'
 
-    timer.innerHTML = tempo
+    });
 }
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let timer;
+let segundos = 0;
+
+function iniciaRelogio(){
+    timer = setInterval(function() {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000)}
+
+    document.addEventListener('click', function(e) {
+        const elemento = e.target;
+
+        if(elemento.classList.contains('iniciar')) {
+            relogio.classList.remove('timer-vermelho');
+            clearInterval(timer);
+            iniciaRelogio();
+        }
+
+        if(elemento.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.add('timer-vermelho');
+        }
+
+        if(elemento.classList.contains('zerar')) {
+            relogio.classList.remove('timer-vermelho')
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            segundos = 0;
+        }
+    })
+}
+
+relogio();
