4e40a8a545e1904f887874888b48055d7a6d643c
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-03-05 04:24:27 -0300

    funçoes

---
+++ aula26_funcoesNovamente/index.js
@@ -0,0 +1,32 @@
+/*
+falaOi();
+function falaOi() {
+    console.log('oi');
+}
+//First-class objscts (objetos de primeira classe)
+//Function
+const souUmDado = function () {
+    console.log('sou um dado.');
+}
+souUmDado();
+
+//Arrow function
+const funcaoArrow = () => {console.log('Sou uma arrow function')}
+funcaoArrow();
+
+//Dentro de objeto eu posso ter uma funcao
+
+const obj = {
+    falar() {
+        console.log('Estou falando....')
+    }
+}
+
+obj.falar()
+*/
+function funcao([abacate, pera, mamao]) {
+
+    console.log(abacate, pera, mamao)
+}
+
+funcao(['maça, banana, caju']);
