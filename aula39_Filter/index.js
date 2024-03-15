ad62ea7ce7ffaa7b13d11a0d983be252a9fcfa48
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-03-14 22:48:58 -0300

    filter and map

--- aula39_Filter/index.js
+++ aula39_Filter/index.js
@@ -1,4 +1,31 @@
 // filter, map, reduce
 
 //Retorne os numeros maiores que 10
+/*
 const numeros = [5,50,80,1,2,3,5,8,7,11,15,22,27];
+const numerosFiltrados = numeros.filter(valor => valor > 10);
+console.log(numerosFiltrados);
+*/
+
+//Retorne as pessoas que tem o nome com 5 letras ou mais
+//Retorne as pessoas com mais de 50 anos
+//Retorne as pessoas cujo nome termina com a
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Leticia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 47},
+];
+
+const nomeCom5Letras = pessoas.filter((obj) => obj.nome.length >= 5);
+console.log(nomeCom5Letras)
+
+const idadeMaiorA50Anos = pessoas.filter((valor) => valor.idade > 50);
+console.log(idadeMaiorA50Anos)
+
+const nomeComA = pessoas.filter(function (valor) {
+    return valor.nome.toLocaleLowerCase().endsWith('a')
+})
+console.log(nomeComA)
---
+++ aula40_map/index.js
@@ -0,0 +1,27 @@
+//Dobre os numeros
+
+const numeros = [5,50,80,1,2,3,5,8,7,11,15,22,27];
+const numerosEmDobro = numeros.map(valor => valor * 2)
+console.log(numerosEmDobro);
+
+// Para cada elemento:
+// Retorne apenas uma string com o nome da pessoa
+// Remova apenas a chave "nome" do objeto
+// Adicione uma chave id em cada objeto
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Leticia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 47},
+];
+
+const soNome = pessoas.map(valor => valor.nome);
+const idades = pessoas.map(objt => ({idade: objt.idade}));
+const id = pessoas.map(function(objt, indice) {
+    const newObjt = {...objt}
+    newObjt.id = indice
+    return newObjt;
+})
+console.log(id);
