ea29989635e943dbdb075bbedee86a5eda158b78
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-03-24 15:01:54 -0300

    metodos

---
+++ aula47_metodos_para_object/index.js
@@ -0,0 +1,17 @@
+/*
+Object.values
+Object.entries
+Object.assign(des, any)
+Object.getOnwPropertyDescriptor(o, 'prop')
+... (spread)
+
+
+// ja vimos
+Object.keys (retorna as chaves)
+Object.freeze (congela o objeto)
+Object.defineProperties (define varias propriedades)
+Object.defineProperty (define uma propriedade)
+*/
+
+const produto = {nome: 'Caneca', preco: 1.80}
+const outraCoisa = produto
