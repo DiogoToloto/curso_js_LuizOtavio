17145c58e0e727a20bcadfc77fa794e389f183ec
Diogo Gabriel <140361866+DiogoToloto@users.noreply.github.com>
2024-04-25 23:39:36 -0300

    manipulando prototype

---
+++ aula49_manipulando_prototype/index.js
@@ -0,0 +1,88 @@
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+};
+
+Produto.prototype.desconto = function(precentual) {
+    this.preco = this.preco - (this.preco) * (precentual / 100);
+};
+
+Produto.prototype.aumento = function(precentual) {
+    this.preco = this.preco + (this.preco) * (precentual / 100);
+};
+
+const p1 = new Produto('Camiseta', 50);
+
+const p2 = {
+    nome: 'Caneca',
+    preco: 15
+};
+Object.setPrototypeOf(p2, Produto.prototype);
+
+p2.aumento(10);
+
+const p3 = Object.create(Produto.prototype, {
+    preco: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 99
+    },
+
+    tamanho: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 42
+    }
+});
+p3.aumento(10);
+console.log(p3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// New object => object.prototype
+//const objA = {
+//    chaveA: 'A'
+    // __proto__: object.prototype
+//};
+
+//const objB = {
+//    chaveB: 'B'
+    //__proto__: objA
+//};
+
+//const objC = new Object();
+//objC.chaveC = 'C';
+
+//Object.setPrototypeOf(objB, objA);
+//Object.setPrototypeOf(objC, objB);
+//console.log(objC.chaveA)
